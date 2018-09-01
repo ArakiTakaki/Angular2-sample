424e4b8f3c2be7fa77f5844c2580832073bb7fee
ArakiTakaki <araki_takaki@yahoo.co.jp>
2018-09-01 22:55:22 +0900

    ヒーロー選択チュートリアル

---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,10 @@
+import {Hero} from './hero';
+
+export const HEROES: Hero[] = [
+  { id:1, name:"アンパンマン"},
+  { id:2, name:"ワンパンマン" },
+  { id:3, name:"オールマイト" },
+  { id:4, name:"スパイダーマン" },
+  { id:5, name:"ラルフ" },
+  { id:6, name:"スパイダーマン" }
+]
